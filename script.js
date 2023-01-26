ebb1d6f68de0f5814dc31a3ca2df25b374604ea2
Gustavo Olimpio <gustavo-olimpio@hotmail.com>
2023-01-25 21:39:26 -0300

    função de pegar quiz no click implementado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let id = "";
 getQuizzes();
 
 function getQuizzes() {
@@ -10,11 +10,12 @@ function ShowQuizzes(response){
     let quizList = response.data;
     const quizzes = document.querySelector('.quizzes')
     quizzes.innerHTML=' '
-
+    
     for(let i = 0 ; i < quizList.length ; i++){
 
         quizzes.innerHTML +=
-        `<div>
+        `<div onclick="enterQuizz(this)">
+            <span class="idQuiz">${quizList[i].id}</span>
             <h1>${quizList[i].title} </h1>
             <img src="${quizList[i].image}"> 
         </div>`
@@ -22,7 +23,15 @@ function ShowQuizzes(response){
     }
 }
 
+function enterQuizz(divElement){
+    id=divElement.querySelector('.idQuiz').innerHTML
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`)
+    promise.then(openQuiz);
+}
 
+function openQuiz(response){
+    console.log(response)
+}
 // Gustavo Aqui
 
 
